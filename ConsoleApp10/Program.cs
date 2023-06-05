673c75067c703a3bc946762dcc06e7d5c028d3d9
unknown <maksimatveev92@mail.ru>
2023-06-05 08:19:16 +0300

    first commit

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите значение a: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите значение b: ");
+double b = double.Parse(Console.ReadLine());
+double result = Math.Pow(a + b, 2);
+Console.WriteLine("({0}+{1})^2={2} при a={0}, b={1}", a, b, result);
